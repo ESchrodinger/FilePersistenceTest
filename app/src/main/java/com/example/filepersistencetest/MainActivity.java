fc9066b2cb00bca543501a249b402fb2fa529546
zjiang.li <1002029005@qq.com>
2023-01-13 17:41:36 +0800

    实现了数据在文件输入输出

--- app/src/main/java/com/example/filepersistencetest/MainActivity.java
+++ app/src/main/java/com/example/filepersistencetest/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
@@ -20,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edit = findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"Restoring succeeded",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -50,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public String load(){
+        FileInputStream in =  null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+
+        try{
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while((line = reader.readLine()) != null){
+                content.append(line);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (reader != null){
+                try{
+                    reader.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
